fe3862ad7b0c337421ccb81072a2000df05b9f0a
thutasann <thutasann2002@gmail.com>
2023-02-05 16:23:59 +0630

    feature: Drawing logic

--- Frontend/src/components/Whiteboard/index.tsx
+++ Frontend/src/components/Whiteboard/index.tsx
@@ -1,7 +1,73 @@
-import React from 'react'
+import { useEffect, useState, useLayoutEffect } from 'react'
+import rough from 'roughjs'
 
-const WhiteBoard = () => {
-  return <canvas width='100%' height='100%'></canvas>
+// const roughGenerator = rough.generator()
+
+const WhiteBoard = ({ canvasRef, ctxRef, elements, setElements }) => {
+  const [isDrawing, setIsDrawing] = useState<boolean>(false)
+
+  useEffect(() => {
+    const canvas = canvasRef.current
+    const ctx = canvas.getContext('2d')
+    ctxRef.current = ctx
+  }, [])
+
+  useLayoutEffect(() => {
+    const roughCanvas = rough.canvas(canvasRef.current)
+    elements.forEach(element => {
+      roughCanvas.linearPath(element.path)
+    })
+  }, [elements])
+
+  const handleMouseDown = (e: any) => {
+    const { offsetX, offsetY } = e.nativeEvent
+    setElements(prevElements => [
+      ...prevElements,
+      {
+        type: 'pencil',
+        offsetX,
+        offsetY,
+        path: [[offsetX, offsetY]],
+        stroke: 'black',
+      },
+    ])
+    setIsDrawing(true)
+  }
+
+  const handleMouseMove = (e: any) => {
+    const { offsetX, offsetY } = e.nativeEvent
+    if (isDrawing) {
+      const { path } = elements[elements.length - 1]
+      const newPath = [...path, [offsetX, offsetY]]
+
+      setElements(prevElements =>
+        prevElements.map((ele, index) => {
+          if (index === elements.length - 1) {
+            return {
+              ...ele,
+              path: newPath,
+            }
+          } else {
+            return ele
+          }
+        })
+      )
+    }
+  }
+
+  const handleMouseUp = (e: any) => {
+    setIsDrawing(false)
+  }
+
+  return (
+    <canvas
+      ref={canvasRef}
+      className='w-full h-full'
+      onMouseDown={handleMouseDown}
+      onMouseMove={handleMouseMove}
+      onMouseUp={handleMouseUp}
+    ></canvas>
+  )
 }
 
 export default WhiteBoard
