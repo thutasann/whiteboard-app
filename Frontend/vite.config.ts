761ba5cbe995ea26b3793169dff76b524b7461db
thutasann <thutasann2002@gmail.com>
2023-02-05 18:37:08 +0630

    Fix: vite config fix

--- Frontend/vite.config.ts
+++ Frontend/vite.config.ts
@@ -1,9 +1,14 @@
 import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import reactJS from '@vitejs/plugin-react'
+import viteTsconfigPaths from 'vite-tsconfig-paths'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [reactJS(), viteTsconfigPaths()],
+  build: {
+    outDir: 'build',
+    sourcemap: false,
+  },
   server: { open: true, host: true, port: 3000 },
   preview: { port: 3300 },
 })
